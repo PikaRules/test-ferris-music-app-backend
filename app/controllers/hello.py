9bdd6314ba7b305f20146bb9c6486a3509aefa4a
guillermo martel <martelgames@gmail.com>
2014-08-01 15:04:31 -0600

    adf

--- app/controllers/hello.py
+++ app/controllers/hello.py
@@ -1,8 +1,28 @@
 from ferris import Controller, route, messages
+from app.models.songtest import Songtest
 
 class Hello(Controller):
 
 	def list(self):
+
+		salida = ""
+		song1 = Songtest( title = "song1" )
+		song1.put()
+		song2 = Songtest( title = "song2" )
+		song2.put()
+		song3 = Songtest( title = "song3" )
+		song3.put()
+		song4 = Songtest( title = "song4" )
+		song4.put()
+
+		listOfSongs = []
+		listOfSongs.append( song1 )
+		listOfSongs.append( song2 )
+
+		songs = ndb.gql( " SELECT * FROM Songtest" )
+
+		cosa = message.to_message( song1, SongtestMessage )
+
 		return "<h1> hello </h1>"
 
 	def queryTest():
@@ -22,10 +42,9 @@ class Hello(Controller):
 
 		songs = ndb.gql( " SELECT * FROM Songtest" )
 
-		JsonSong = model_message()
+		cosa = message.to_message( song1, SongtestMessage )
 
 		#print songs.count()
 		#print songs
 		#print listOfSongs
 
-		return salida
--- app/models/songtest.py
+++ app/models/songtest.py
@@ -1,5 +1,10 @@
-from ferris import BasicModel
+from ferris import BasicModel, messages
 from google.appengine.ext import ndb
 
 class Songtest(BasicModel):
 	title = ndb.StringProperty(required=True)
+
+class SongtestMessage(message.Message):
+	title = message.StringField(1)
+	
+
