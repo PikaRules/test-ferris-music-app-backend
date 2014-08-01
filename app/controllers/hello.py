a15142254b2c36043d066f71e620e3f9d186fce4
guillermo martel <martelgames@gmail.com>
2014-08-01 15:25:00 -0600

    cambios

--- app/controllers/hello.py
+++ app/controllers/hello.py
@@ -1,11 +1,14 @@
 from ferris import Controller, route, messages
+from google.appengine.ext import ndb
 from app.models.songtest import Songtest
 
 class Hello(Controller):
+	class Meta:
+			View = 'json'
+
 
 	def list(self):
 
-		salida = ""
 		song1 = Songtest( title = "song1" )
 		song1.put()
 		song2 = Songtest( title = "song2" )
@@ -21,30 +24,10 @@ class Hello(Controller):
 
 		songs = ndb.gql( " SELECT * FROM Songtest" )
 
-		cosa = message.to_message( song1, SongtestMessage )
+		self.context['data'] = songs
 
-		return "<h1> hello </h1>"
+	@route	
+	def cosa(self):
+		return "hey"
 
-	def queryTest():
-		salida = ""
-		song1 = Songtest( title = "song1" )
-		song1.put()
-		song2 = Songtest( title = "song2" )
-		song2.put()
-		song3 = Songtest( title = "song3" )
-		song3.put()
-		song4 = Songtest( title = "song4" )
-		song4.put()
-
-		listOfSongs = []
-		listOfSongs.append( song1 )
-		listOfSongs.append( song2 )
-
-		songs = ndb.gql( " SELECT * FROM Songtest" )
-
-		cosa = message.to_message( song1, SongtestMessage )
-
-		#print songs.count()
-		#print songs
-		#print listOfSongs
-
+	
--- app/models/songtest.py
+++ app/models/songtest.py
@@ -4,7 +4,4 @@ from google.appengine.ext import ndb
 class Songtest(BasicModel):
 	title = ndb.StringProperty(required=True)
 
-class SongtestMessage(message.Message):
-	title = message.StringField(1)
-	
 
