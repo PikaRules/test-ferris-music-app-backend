c98a0993d93f502dfb04dc147012c71ca3b5d785
Guillermo Martel <martelgames@gmail.com>
2014-08-07 00:25:14 -0600

    artist model

---
+++ app/models/artist.py
@@ -0,0 +1,16 @@
+from ferris import BasicModel, ndb
+
+class Artist(BasicModel)
+	name = ndb.StringProperty(required=True)
+	sex = ndb.StringProperty( choices=( 'male','female' ) )
+	description = ndb.StringProperty()
+
+	@staticmethod
+	def all():
+		items = ndb.gql( " SELECT * FROM Artist" )
+		return items
+
+	@staticmethod
+	def find_by_name( name ):
+		result = ndb.gql( " SELECT * FROM Artist WHERE name = :1", name )
+		return result
