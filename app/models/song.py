87e8ff83e1215ee06478c614c4bf8bc545795cf2
Guillermo Martel <martelgames@gmail.com>
2014-09-03 21:41:54 -0600

    testing tasklets

--- app/models/song.py
+++ app/models/song.py
@@ -12,26 +12,41 @@ class Song(BasicModel):
 
 	@classmethod
 	def all(self):
-
-		msong = {
-				"title": "",
-				"description": "",
-				"artist": ""
-			}
-
 		query_songs = ndb.gql( " SELECT * FROM Song" )
-
+		songs = query_songs.map( self.all_callback, limit=100 )
 		return songs
 
 	@classmethod
 	def find_by_title(self,title):
-		songs = ndb.gql( " SELECT * FROM Song WHERE title = :1", title )
+		query_songs = ndb.gql( " SELECT * FROM Song WHERE title = :1", title )
+		songs = query_songs.map( self.all_callback, limit=100 )
 		return songs
 
     #https://developers.google.com/appengine/docs/python/ndb/async#tasklets
-
-
-	@ndg.tasklet
+	@ndb.tasklet
 	def all_callback(song):
-		tartist = yield song.artist.get_async()
-		raise ndb.Return(artist)
+		msong = {}
+		if song.artist is not None:
+			tartist = yield song.artist.get_async()
+			msong =  parse_song( song, tartist )
+			raise ndb.Return(msong)
+		else:
+			msong = parse_song( song, None )
+			raise ndb.Return(msong)
+
+
+def parse_song( song , artist ):
+	msong = {}
+	martist = {}
+	if artist is not None:
+		martist["__kind__"] = artist.key.kind()
+		martist["__id__"] = artist.key.id()
+		martist["name"] = artist.name
+		martist["description"] = artist.description
+		martist["sex"] = artist.sex
+	msong["artist"] = martist
+	msong["__kind__"] = song.key.kind()
+	msong["__id__"] = song.key.id()
+	msong["title"] = song.title
+	msong["description"] = song.description
+	return msong
